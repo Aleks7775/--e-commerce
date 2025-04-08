2f59f44636ff60c2e67aec3e76f54bdb50dc9798
Aleksandr <feat9999@yandex.ru>
2025-04-08 18:36:40 +0400

    add function category and product

---
+++ src/category.py
@@ -0,0 +1,16 @@
+from src.product import Product
+
+
+class Category:
+    name: str
+    description: str
+    product: list
+    quantity_categories = 0
+    quantity_product = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.quantity_categories += 1
+        Category.quantity_product += len(products)
