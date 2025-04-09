fa355ba415afd700f7ed237fb9d0cf0b36f39f7f
Aleksandr <feat9999@yandex.ru>
2025-04-09 14:42:19 +0400

    improvements

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,11 @@ from src.product import Product
 def first_category():
     return Category (
         name="Смартфоны",
-        description="Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
-        products=[Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+        description="Смартфоны, как средство не только коммуникации, "
+                    "но и получение дополнительных функций для удобства жизни",
+        products=[Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, "
+                                                      "200MP камера", 180000.0, 5),
                   Product("Iphone 15", "2512GB, Gray space", 210000.0, 14)]
-
         )
 
 
@@ -19,10 +20,12 @@ def first_category():
 def second_category():
     return Category(
         name="Телевизоры",
-        description="Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        description="Современный телевизор, который позволяет наслаждаться просмотром, "
+                    "станет вашим другом и помощником",
         products=[Product("55 QLED 4K", "Фоновая подсветка", 123000.0, 7)]
 
     )
+
 
 @pytest.fixture
 def product():
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,8 @@
 
 def test_product_init(first_category, second_category):
     assert first_category.name == "Смартфоны"
-    assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
+    assert first_category.description == ("Смартфоны, как средство не только коммуникации, "
+                                          "но и получение дополнительных функций для удобства жизни")
     assert len(first_category.products) == 2
 
     assert first_category.quantity_categories == 2
