8d4302dc42bc20863efcc3665bd621045651de0d
Aleksandr <feat9999@yandex.ru>
2025-04-11 22:00:16 +0400

    Homework 14.2 New functionality and tests for it

--- src/product.py
+++ src/product.py
@@ -7,5 +7,23 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data):
+        """класс-метод, который будет принимать на вход параметры товара
+         в словаре и возвращать созданный объект класса Product"""
+        return cls(**product_data)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Проверка в случае если цена равна или ниже нуля, выводите сообщение в консоль"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
