fc990d6f23efa08464cac10133fd51a9886b37b4
Aleksandr <feat9999@yandex.ru>
2025-04-15 16:56:23 +0400

    new tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,3 +30,8 @@ def second_category():
 @pytest.fixture
 def product():
     return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture
+def product2():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
--- tests/test_category.py
+++ tests/test_category.py
@@ -15,3 +15,8 @@ def test_product_init(first_category, second_category):
 def test_product_list_property(first_category):
     assert first_category.products == ("Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт."
                                        "\nIphone 15, 210000.0 руб. Остаток: 14 шт.\n")
+
+
+def test_category_str(first_category):
+    """Тест для проверки отображения правильного название и общее количество продуктов"""
+    assert str(first_category) == "Смартфоны, количество продуктов: 2 шт."
